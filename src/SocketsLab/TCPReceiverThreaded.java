2aeecea4d3568cb7c243a85ae335ad1b4651ff97
Tom <tom.hoad03@gmail.com>
2021-02-16 12:13:09 +0000

    First attempts at acknowledgments

--- src/SocketsLab/TCPReceiverThreaded.java
+++ src/SocketsLab/TCPReceiverThreaded.java
@@ -2,24 +2,39 @@ package SocketsLab;
 
 import java.io.*;
 import java.net.*;
-class TCPReceiverThreaded{
-    public static void main(String [] args){
-        try{
+class TCPReceiverThreaded {
+    public static void main(String[] args) {
+        try {
             ServerSocket ss = new ServerSocket(4322);
-            for(;;){
-                try{final Socket client = ss.accept();
-                    new Thread(new Runnable(){
-                        public void run(){try{
-                            BufferedReader in = new BufferedReader(
-                                    new InputStreamReader(client.getInputStream()));
-                            String line;
-                            while((line = in.readLine()) != null)
-                                System.out.println(line+" received");
-                            client.close(); }catch(Exception e){}
+            for (; ; ) {
+                try {
+                    final Socket client = ss.accept();
+                    new Thread(new Runnable() {
+                        public void run() {
+                            try {
+                                BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                                PrintWriter out = new PrintWriter(client.getOutputStream());
+                                out.println("Acknowledged"); out.flush();
+
+                                String line;
+                                while ((line = in.readLine()) != null)
+                                    System.out.println(line + " received from " + client.getInetAddress());
+                                    Thread.sleep(1000);
+                                    out.println("Acknowledged"); out.flush();
+                                    System.out.println("Acknowledged from " + client.getInetAddress());
+                                client.close();
+                            } catch (Exception e) {
+                            }
                         }
                     }).start();
-                }catch(Exception e){System.out.println("error "+e);}
+                } catch (Exception e) {
+                    System.out.println("error " + e);
+                }
             }
-        }catch(Exception e){System.out.println("error "+e);}
+        } catch (Exception e) {
+            {
+                System.out.println("error " + e);
+            }
+        }
     }
 }
--- src/SocketsLab/TCPSender.java
+++ src/SocketsLab/TCPSender.java
@@ -8,10 +8,16 @@ class TCPSender {
         try {
             Socket socket = new Socket("Tom-Desktop",4322);
             PrintWriter out = new PrintWriter(socket.getOutputStream());
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
             for(int i=0;i<25;i++) {
-                out.println("TCP message "+i); out.flush();
-                System.out.println("TCP message "+i+" sent");
-                Thread.sleep(1000);
+                if (in.readLine().equals("Acknowledged")) {
+                    out.println("TCP message " + i); out.flush();
+                    System.out.println("TCP message "+ i +" sent");
+                    Thread.sleep(1000);
+                } else {
+                    out.println("No acknowledgement from " + socket.getInetAddress());
+                }
             }
         }
         catch (Exception e) {
